25f1e7c0eb079cc3a52755edf97839a86f6c0053
Maysa Orazova <[maysaorash@gmail.com]>
2021-03-04 19:12:46 -0500

    added env file and some examples

--- myApp.js
+++ myApp.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var app = express();
+
 // console.log("Hello World")
 
 // app.get("/", (req,res)=>{
 // 	res.send("Hello Express")
 // })
 
-app.get("/", (request, response) => response.sendFile(__dirname + "/views/index.html"));
+// app.get("/", (request, response) => response.sendFile(__dirname + "/views/index.html"));
+
+
+// app.get("/json", (request, response) => response.json({"message": "Hello json"}));
+
+
+// app.get("/json",(req,res)=>{process.env.MESSAGE_STYLE=="uppercase"?res.json({"message": "Hello json".toUpperCase()}) : res.json({"message": "Hello json"})});
+
+
+
+
+app.use((req, res, next)=> {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
+
+
+
 
 
 
